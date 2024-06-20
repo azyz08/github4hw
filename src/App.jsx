6e3e521f82c8be54f48f0945993a9998dae41f38
azyz08 <ibroximova014@gmail.com>
2024-06-20 12:02:45 +0500

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
       <h1>Abduxabir</h1>
 
 
-      <h1>Azizbek</h1>
+      <div className="light">
+        <img src="/01.png" alt="" />
+        <div className="text">
+          <h1>Light, Fast & Powerful</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br /> Aenean commodo ligula eget dolor. Aenean massa. Cum  <br /> sociis natoque penatibus et magnis dis parturient montes, <br /> nascetur ridiculus </p>
+          <p>mus. Donec quam felis, ultricies nec, pellentesque eu, pretium <br /> quis, sem. Nulla consequat massa quis enim.</p>
+        </div>
+      </div>
     </>
   )
 }
